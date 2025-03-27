a04b7abdbcae339aebdfaa0c7620b3dc4e298adc
Willy <willy548798@gmail.com>
2025-03-27 23:13:31 +0800

    Initial Commit

---
+++ tip-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by Willy Hsu on 2025/3/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
