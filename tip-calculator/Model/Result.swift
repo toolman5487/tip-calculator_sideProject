17b20b629874764ebc6fb23f85d2622d872ca67e
Willy <willy548798@gmail.com>
2025-03-30 18:43:50 +0800

    建立 計算機ViewModels

---
+++ tip-calculator/Model/Result.swift
@@ -0,0 +1,8 @@
+//
+//  Result.swift
+//  tip-calculator
+//
+//  Created by Willy Hsu on 2025/3/30.
+//
+
+import Foundation
---
+++ tip-calculator/ViewModels/CalculatorVM.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorVM.swift
+//  tip-calculator
+//
+//  Created by Willy Hsu on 2025/3/30.
+//
+
+import Foundation
