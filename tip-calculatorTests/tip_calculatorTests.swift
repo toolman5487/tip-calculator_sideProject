2e99f1e40b2b91335bc58c28fb660ed5cc565064
Willy <willy548798@gmail.com>
2025-04-07 15:04:04 +0800

    - Write more tests         - testResultWithoutTipFor2Person         - testResultWith10PercentTipFor2Person         - testResultWithCustomTipFor4Person

--- tip-calculatorTests/tip_calculatorTests.swift
+++ tip-calculatorTests/tip_calculatorTests.swift
@@ -22,8 +22,15 @@ final class tip_calculatorTests: XCTestCase {
         super.setUp()
     }
     
-    func testResultWithoutTipFor_1_Person(){
-        let bill:Double = 100
+    func buildInput(bill:Double, tip:Tip, split: Int) -> CalculatorVM.Input {
+        return .init(billPublisher: Just(bill).eraseToAnyPublisher(),
+                     tipPublisher: Just(tip).eraseToAnyPublisher(),
+                     splitPublisher: Just(split).eraseToAnyPublisher(),
+                     logoViewTapPulisher: logoViewTapSubject.eraseToAnyPublisher())
+    }
+    
+    func testResultWithoutTipFor_1Person(){
+        let bill:Double = 100.0
         let tip:Tip = .none
         let split:Int = 1
         let input = buildInput(bill: bill, tip: tip, split: split)
@@ -35,11 +42,43 @@ final class tip_calculatorTests: XCTestCase {
         }.store(in: &cancellables)
     }
     
-    func buildInput(bill:Double, tip:Tip, split: Int) -> CalculatorVM.Input {
-        return .init(billPublisher: Just(bill).eraseToAnyPublisher(),
-                     tipPublisher: Just(tip).eraseToAnyPublisher(),
-                     splitPublisher: Just(split).eraseToAnyPublisher(),
-                     logoViewTapPulisher: logoViewTapSubject.eraseToAnyPublisher())
+    func testResultWithoutTipFor_2Person(){
+        let bill:Double = 100
+        let tip:Tip = .none
+        let split:Int = 2
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        let output = sut.tranform(input: input)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 50.0)
+            XCTAssertEqual(result.totalBill, 100)
+            XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWith_10PercentTip_For_2Person(){
+        let bill:Double = 100
+        let tip:Tip = .tenPercent
+        let split:Int = 2
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        let output = sut.tranform(input: input)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 55.0)
+            XCTAssertEqual(result.totalBill, 110)
+            XCTAssertEqual(result.totalTip, 10)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWithCustomTip_For_4Person(){
+        let bill:Double = 100
+        let tip:Tip = .custom(value: 60)
+        let split:Int = 4
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        let output = sut.tranform(input: input)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 40.0)
+            XCTAssertEqual(result.totalBill, 160)
+            XCTAssertEqual(result.totalTip, 60)
+        }.store(in: &cancellables)
     }
     
     override func tearDown() {
