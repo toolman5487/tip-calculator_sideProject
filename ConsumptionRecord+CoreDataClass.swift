1901ae968872f78f7a925f96f466ef452c892c7c
Willy Hsu <hsunefer@gmail.com>
2026-02-05 16:50:14 +0800

    feat: 新增 CoreData

---
+++ ConsumptionRecord+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  ConsumptionRecord+CoreDataClass.swift
+//  tip-calculator
+//
+//  Created by Willy Hsu on 2026/2/5.
+//
+//
+
+public import Foundation
+public import CoreData
+
+public typealias ConsumptionRecordCoreDataClassSet = NSSet
+
+@objc(ConsumptionRecord)
+public class ConsumptionRecord: NSManagedObject {
+
+}
---
+++ ConsumptionRecord+CoreDataProperties.swift
@@ -0,0 +1,34 @@
+//
+//  ConsumptionRecord+CoreDataProperties.swift
+//  tip-calculator
+//
+//  Created by Willy Hsu on 2026/2/5.
+//
+//
+
+public import Foundation
+public import CoreData
+
+
+public typealias ConsumptionRecordCoreDataPropertiesSet = NSSet
+
+extension ConsumptionRecord {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ConsumptionRecord> {
+        return NSFetchRequest<ConsumptionRecord>(entityName: "ConsumptionRecord")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var createdAt: Date?
+    @NSManaged public var bill: Double
+    @NSManaged public var totalTip: Double
+    @NSManaged public var totalBill: Double
+    @NSManaged public var amountPerPerson: Double
+    @NSManaged public var split: Int16
+    @NSManaged public var tipRawValue: String?
+
+}
+
+extension ConsumptionRecord : Identifiable {
+
+}
