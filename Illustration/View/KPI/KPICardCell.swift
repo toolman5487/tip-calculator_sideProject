850218e333880a08f2d667d267e67055c45110f8
Willy <willy548798@gmail.com>
2026-02-13 17:56:20 +0800

    feat: 修改總覽樣式

--- Illustration/View/KPI/KPICardCell.swift
+++ Illustration/View/KPI/KPICardCell.swift
@@ -12,15 +12,20 @@ final class KPICardCell: UICollectionViewCell {
 
     private let containerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .clear
+        view.layer.borderWidth = 2
+        view.layer.borderColor = UIColor.label.cgColor
         view.layer.cornerRadius = 12
+        view.clipsToBounds = true
         return view
     }()
 
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.font = ThemeFont.regular(Ofsize: 12)
-        label.textColor = .secondaryLabel
+        label.textColor = .systemBackground
+        label.textAlignment = .center
+        label.backgroundColor = .label
         return label
     }()
 
@@ -28,6 +33,9 @@ final class KPICardCell: UICollectionViewCell {
         let label = UILabel()
         label.font = ThemeFont.bold(Ofsize: 20)
         label.textColor = ThemeColor.text
+        label.textAlignment = .center
+        label.textColor = .label
+        label.backgroundColor = .systemBackground
         return label
     }()
 
