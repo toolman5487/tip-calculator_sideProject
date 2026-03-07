872e86925f3da046e2f32409be5a9b06f419b00b
Willy <willy548798@gmail.com>
2026-03-07 11:22:49 +0800

    feat: 調整主題色

--- tip-calculator/Config/ThemeColor.swift
+++ tip-calculator/Config/ThemeColor.swift
@@ -11,11 +11,11 @@ import UIKit
 struct ThemeColor {
     static let bg = UIColor.systemGroupedBackground
     static let primary = UIColor(hexString: "000000")
-    static let secondary = UIColor(hexString: "E53935")
+    static let secondary = UIColor(hexString: "E67E22")
     static let text = UIColor(hexString: "000000")
     static let seperator = UIColor(hexString: "CCCCCC")
 
-    static let trendUp = UIColor(hexString: "E53935")
-    static let trendDown = UIColor(hexString: "43A047")
-    static let trendFlat = UIColor(hexString: "F9A825")
+    static let trendUp = UIColor(hexString: "D32F2F")
+    static let trendDown = UIColor(hexString: "137333")
+    static let trendFlat = UIColor(hexString: "E67E22")
 }
